b0652c6a18ccf59106c060a40adb5a2034238e13
chrismclarke <chris.m.clarke@live.co.uk>
2023-01-31 10:37:20 -0800

    chore: add yarn, ts, update readme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+/** @type {import('next').NextConfig} */
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
 });
 
 module.exports = withNextra();
