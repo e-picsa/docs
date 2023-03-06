fa8849d5af37823396d6f4f5275365243f6a919e
Chris <chris.m.clarke@live.co.uk>
2023-03-06 10:52:48 +0000

    ci: update gh pages deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const withNextra = require("nextra")({
+  images: {
+    // required to deploy to github pages
+    unoptimized: true,
+  },
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
